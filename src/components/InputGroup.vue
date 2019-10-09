800bf8fc1cc157eb33cbb41d59beb0f4893db250
sunny <2515762599@qq.com>
2019-10-09 18:09:58 +0800

    登陆完成

--- src/components/InputGroup.vue
+++ src/components/InputGroup.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="inputgroup">
-        <input
-            type="text"
-            :placeholder="placeholder"
-            @input="$emit('input', $event.target.value)"
-        />
-        <button v-if="text">{{ text }}</button>
+    <div class="inpBox">
+        <div class="inputgroup" :class="{'redBorder' : error}">
+            <input
+                type="text"
+                :placeholder="placeholder"
+                @input="$emit('input', $event.target.value)"
+            />
+            <button v-if="text" @click="$emit('btnClick')" :disabled = 'disabled'>{{ text }}</button>
+        </div>
+        <div class="error" v-show="error">{{error}}</div>
     </div>
 </template>
 
 <script>
 export default {
     name: "inputGroup",
-    props: ["placeholder", "text", "phone"],
+    props: ["placeholder", "text", "phone", "error","disabled"],
     data() {
         return {};
     }
@@ -20,22 +23,39 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.inputgroup {
-    border: 1px solid #ccc;
+.inpBox {
+    position: relative;
     width: 70%;
+}
+.inputgroup {
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    width: 100%;
     line-height: 30px;
     box-sizing: border-box;
     padding: 0 0 0 10px;
-    margin-top: 20px;
+    margin-bottom: 30px;
     input {
         border: none;
         outline: none;
         font-size: 14px;
+        width: 66%;
     }
     button {
+        margin-right: 2px;
         border: none;
         outline: none;
         background-color: #fff;
     }
+}
+.redBorder {
+    border-color: red;
+}
+.error {
+    width: 70%;
+    font-size: 14px;
+    color: red;
+    position: absolute;
+    bottom: 8px;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,38 +1,131 @@
 <template>
     <div class="login">
-        <h2>login</h2>
-        <div class="inp">
-            <inputGroup placeholder='请输入手机号' text='获取验证码' v-model="phone"/>
-            <input-group placeholder="请输入验证码" v-model="verifyCode"/>
+        <div class="img">
+            <img src="../assets/images/logo.jpg" alt />
         </div>
-        <span>{{ phone }}</span>
+        <div class="inp">
+            <inputGroup
+                placeholder="手机号"
+                :text="text"
+                v-model="phone"
+                @btnClick="btnClick"
+                :error="error.phone"
+                :disabled = 'disabled'
+            />
+            <input-group placeholder="验证码" v-model="verifyCode" :error="error.code" />
+        </div>
+        <div class="text">
+            新用户登陆自动注册，表示已同意
+            <span @click="userAgreement">《用户服务协议》</span>
+        </div>
         <button @click="login">登陆</button>
-
     </div>
 </template>
 
 <script>
-import inputGroup from '../components/InputGroup.vue'
-    export default {
-        name:'login',
-        data(){
-            return{
-                phone :'',
-                verifyCode :''
+import inputGroup from "../components/InputGroup.vue";
+export default {
+    name: "login",
+    data() {
+        return {
+            phone: "",
+            verifyCode: "",
+            text: "获取验证码",
+            disabled : false,
+            error: {
+                phone: "",
+                code: ""
+            }
+        };
+    },
+    components: {
+        inputGroup
+    },
+    methods: {
+        login() {
+            console.log(this.phone);
+            this.error.code = "验证码输入错误";
+        },
+        userAgreement() {},
+        btnClick() {
+            if (!this.phone) {
+                //判断手机号是否为空
+                this.error.phone = "手机号不能为空";
+            } else {
+                // 验证手机号的格式是否正确
+                if (this.validatePhone(this.phone)) {
+                    // 通过正则验证如果正确则发送获取验证码的请求
+                    console.log(333);
+                    this.error.phone = "";
+                    // 发送请求 并且触发倒计时函数
+                    this.$axios.post('/apis/posts/sms_send', {
+                        mobile : this.phone,
+                        key : '5c0f0ec145d988ef90c49a9b4b410032',
+                        tpl_id : '190842'
+                    })
+                    .then (res =>{
+                        console.log(res)
+                    })
+                    this.validateTime();
+                } else {
+                    // 格式不正确 提示
+                    this.error.phone = "手机号格式输入错误";
+                }
             }
         },
-        components :{
-            inputGroup
+        validatePhone(num) {
+            let reg = /^1[345678]\d{9}$/;
+            return reg.test(num);
         },
-        methods:{
-            login(){
-                console.log(this.phone)
-            }
-
+        validateTime() {
+            let num = 45;
+            let timer = setInterval(() => {
+                this.disabled = true
+                num--;
+                this.text = `${num}秒后重试`;
+                if (num == 0) {
+                    this.text = "获取验证码";
+                    clearInterval(timer);
+                    this.disabled = false
+                }
+            }, 1000);
         }
     }
+};
 </script>
 
 <style lang="less" scoped>
-
+.login {
+    padding: 40px 10px 0 10px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    .img {
+        // text-align: center;
+        margin-bottom: 20px;
+    }
+    .inp {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+    .text {
+        width: 70%;
+        margin: 0 0 20px 0;
+        font-size: 14px;
+        span {
+            color: rgb(37, 104, 248);
+        }
+    }
+    button {
+        border-radius: 4px;
+        width: 70%;
+        border: none;
+        outline: none;
+        height: 40px;
+        font-size: 16px;
+        color: #fff;
+        background: rgb(24, 163, 54);
+    }
+}
 </style>
